0e7c50c2a2b03648d5216baa5b33a77bdcf39643
Jeffrey <jeff@jjakt.monster>
2024-11-27 07:30:51 -0600

    turned https redirection off in development because it's running containerized and trust takes time.

--- ClientService/Program.cs
+++ ClientService/Program.cs
@@ -17,9 +17,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
+else
+{
+    app.UseHttpsRedirection();
+}
 app.UseAuthorization();
 
 app.MapControllers();
